79f7fd5b8156b65130e5c3dd465c112bc28081be
an978607 <anneb17@knights.ucf.edu>
2021-11-05 18:41:32 -0400

    Main Map (80%) Complete

---
+++ WaterKing/Assets/Scripts/Fuel.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System; 
+
+public class Fuel : MonoBehaviour
+{
+    [SerializeField] Text energyText;
+    [SerializeField] Text timerText;
+
+    private int maxEnergy = 3;
+    private int currentEnergy;
+    private int restoreDuration;
+
+    private DateTime nextEnergyTime;
+    private DateTime lastEnergyTime;
+
+    private bool isRestoring = false;
+    private PlayerDataManager playerDataManager;
+
+    private void Awake()
+    {
+        playerDataManager = gameObject.GetComponent<PlayerDataManager>();
+    }
+
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("currentEnergy"))
+        {
+            PlayerPrefs.SetInt("currentEnergy", 3);
+            Load();
+            StartCoroutine(RestoreEnergy());
+        }
+        else 
+        {
+            Load();
+            StartCoroutine(RestoreEnergy());
+        }
+    }
+
+    public void UseEnergy()
+    {
+        if(currentEnergy >= 1)
+        {
+            currentEnergy--;
+            UpdateEnergy();
+            if(isRestoring == false)
+            {
+                if(currentEnergy + 1 == maxEnergy)
+                {
+                    nextEnergyTime = AddDurration(DateTime.Now, restoreDuration);
+                }
+
+                StartCoroutine(RestoreEnergy());
+            }
+        }
+
+        else 
+        {
+            Debug.Log("Insufficient Energy");
+        }
+    }
+
+    private IEnumerator RestoreEnergy()
+    {
+        UpdateEnergyTimer();
+        isRestoring = true; 
+
+        while(currentEnergy < maxEnergy)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            DateTime nextDateTime = nextEnergyTime;
+            bool isEnergyAdding = false;
+
+            while(currentDateTime > nextDateTime )
+            {
+                if(currentEnergy < maxEnergy)
+                {
+                    isEnergyAdding = true;
+                    currentEnergy++;
+                    UpdateEnergy();
+                    DateTime timeToAdd = lastEnergyTime > nextDateTime ? lastEnergyTime : nextDateTime;
+                    nextDateTime = AddDurration(timeToAdd, restoreDuration);
+                }
+
+                else 
+                {
+                    break;
+                }
+            }
+            if(isEnergyAdding == true)
+            {
+                lastEnergyTime = DateTime.Now;
+                nextEnergyTime = nextDateTime;
+            }
+
+            UpdateEnergyTimer();
+            UpdateEnergy();
+            Save();
+            yield return null;
+        }
+        isRestoring = false;
+    }
+
+    private DateTime AddDurration(DateTime datetime, int duration)
+    {
+        return datetime.AddMinutes(30);
+    }
+
+    private void UpdateEnergy()
+    {
+        energyText.text = currentEnergy.ToString() + "/" + maxEnergy.ToString();
+    }
+
+    private void UpdateEnergyTimer()
+    {
+        if(currentEnergy >= maxEnergy)
+        {
+            timerText.text = "FULL";
+            return;
+        }
+
+        TimeSpan time = nextEnergyTime - DateTime.Now;
+        string timeValue = String.Format("{0:00}:{1:00}", time.Minutes, time.Seconds);
+        timerText.text = timeValue;
+    }
+
+    private DateTime StringtoDate(string datetime) 
+    { 
+        if(String.IsNullOrEmpty(datetime))
+        {
+            return DateTime.Now;
+        }
+        else 
+        {
+            return DateTime.Parse(datetime);
+        }
+    }
+
+    private void Load()
+    {
+        currentEnergy = playerDataManager.GetFuelAmount();
+        nextEnergyTime = StringtoDate(PlayerPrefs.GetString("nextEnergyTime"));
+        lastEnergyTime = playerDataManager.GetLastEnergyUpdateTime();
+
+    }
+
+    private void Save()
+    {
+        playerDataManager.SetFuelAmount(currentEnergy);
+        PlayerPrefs.SetString("nextEnergyTime", nextEnergyTime.ToString());
+        playerDataManager.SetLastEnergyUpdateTime(lastEnergyTime);
+    }
+}
