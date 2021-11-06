4ea3b260fb8d99574615abe80e4f809304e24825
an978607 <anneb17@knights.ucf.edu>
2021-11-06 07:31:46 -0400

    Enemy Prefabs
    All enemies added, Game Scene UI works as intended

---
+++ WaterKing/Assets/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // the type of enemies that it can spawn
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject brandMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // select a random float from 0 to 3
+        float delay = Random.Range(0f, 3f);
+        
+        // calls SpawnObject, "delay" randomly delay the first enemy for 0 to 3 seconds
+        Invoke("SpawnObject", delay);
+    }
+
+    public void SpawnObject()
+    {
+            int type = Random.Range(1, 4);
+
+        if (brandMenu.activeSelf == false)
+        {
+            // spawns the enemy
+            switch (type)
+            {
+                case 1:
+                    Instantiate(spawn1, transform.position, transform.rotation);
+                    break;
+
+                case 2:
+                    Instantiate(spawn2, transform.position, transform.rotation);
+                    break;
+
+                case 3:
+                    Instantiate(spawn3, transform.position, transform.rotation);
+                    break;
+            }
+        }
+            // select a random float from 1 to 3
+            float delay = Random.Range(1f, 3f);
+
+            // recursive call SpawnObject, "delay" randomly deplay the first enemy for 1 to 3 seconds
+            Invoke("SpawnObject", delay);
+        }
+}
---
+++ WaterKing/Assets/Scripts/enemy_movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+    score call;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        call = FindObjectOfType<score>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moves enemy object to the left
+        if(Time.timeScale == 1f)
+        {
+            transform.Translate((float)-0.07, 0, 0);
+        }
+
+        // if it gets off screen it will remove the object
+        if(transform.position.x <= -15)
+        {
+            call.Enemy_Destroyed();
+            Destroy(this.gameObject);
+        }
+    }
+}
