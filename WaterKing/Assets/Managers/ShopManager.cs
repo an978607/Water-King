101f8f8cedcc7ca0b133379d6a3494a5b5cdd8f7
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-11-07 13:02:18 -0500

    Add shop confirm purchase

--- WaterKing/Assets/Managers/ShopManager.cs
+++ WaterKing/Assets/Managers/ShopManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    public void TryPurchase()
+    private void TryPurchase()
     {
         
         int playerCurrency = PlayerDataManager.GetCurrency();
@@ -16,6 +16,22 @@ public class ShopManager : MonoBehaviour
         Button button = gameObject.GetComponent<Button>();
         Text buttonText = gameObject.GetComponentInChildren<Text>();
         string parentPrefabTag = gameObject.transform.parent.tag;
+
+        Transform shopUITransform = gameObject.transform.root.Find("Shop UI");
+        if (shopUITransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find shop UI transform");
+            return;
+        }
+
+        Transform confirmPurchaseTransform = shopUITransform.Find("Confirm Purchase");
+        if (confirmPurchaseTransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find confirm purchase transform");
+            return;
+        }
+
+        confirmPurchaseTransform.gameObject.SetActive(false);
 
         switch (parentPanelObject.name)
         {
@@ -83,7 +99,6 @@ public class ShopManager : MonoBehaviour
             default:
                 return;
         }
-
     }
 
     private void UpdateCurrencyUI()
@@ -104,5 +119,114 @@ public class ShopManager : MonoBehaviour
         }
         
         currencyText.text = PlayerDataManager.GetCurrency().ToString();
+    }
+
+    public void AskConfirmPurchase()
+    {
+        Transform backgroundTransform = GetConfirmBackgroundTransform();
+        if (backgroundTransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to get background transform");
+            return;
+        }
+
+        Button confirmButton = GetConfirmButton(backgroundTransform);
+        if (confirmButton == null)
+        {
+            Debug.LogError("ShopManager: Unable to get confirm button");
+            return;
+        }
+
+        Button closeButton = GetCloseConfirmButton(backgroundTransform);
+        if (closeButton == null)
+        {
+            Debug.LogError("ShopManager: Unable to get close button");
+            return;
+        }
+
+        Transform currentCurrencyTransform = backgroundTransform.Find("Current");
+        if (currentCurrencyTransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to get current currency transform");
+            return;
+        }
+        int playerCurrency = PlayerDataManager.GetCurrency();
+        currentCurrencyTransform.GetComponent<Text>().text = playerCurrency.ToString();
+
+        Transform potentialCurrencyTransform = backgroundTransform.Find("Potential");
+        if (potentialCurrencyTransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to get potential currency transform");
+            return;
+        }
+
+        int price = int.Parse(gameObject.GetComponentInChildren<Text>().text);
+        int potentialPrice = playerCurrency - price;
+        potentialCurrencyTransform.GetComponent<Text>().text = potentialPrice.ToString();
+
+        confirmButton.onClick.AddListener(PurchaseConfirmed);
+        confirmButton.onClick.AddListener(delegate { RemoveConfirmScreenButtonListeners(confirmButton, closeButton); });
+        closeButton.onClick.AddListener(delegate {RemoveConfirmScreenButtonListeners(confirmButton, closeButton);});
+    }
+
+    private Transform GetConfirmBackgroundTransform()
+    {
+        Transform shopUITransform = gameObject.transform.root.Find("Shop UI");
+        if (shopUITransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find shop UI transform");
+            return null;
+        }
+
+        Transform confirmPurchaseTransform = shopUITransform.Find("Confirm Purchase");
+        if (confirmPurchaseTransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find confirm purchase transform");
+            return null;
+        }
+
+        confirmPurchaseTransform.gameObject.SetActive(true);
+
+        Transform backgroundTransform = confirmPurchaseTransform.Find("Background");
+        if (backgroundTransform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find background transform");
+            return null;
+        }
+        return backgroundTransform;
+    }
+
+    private Button GetConfirmButton(Transform backgroundTransform)
+    {
+        Transform confirmTranform = backgroundTransform.Find("Confirm");
+        if (confirmTranform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find confirm transform");
+            return null;
+        }
+
+        return confirmTranform.gameObject.GetComponent<Button>();
+    }
+
+    private Button GetCloseConfirmButton(Transform backgroundTransform)
+    {
+        Transform closeTranform = backgroundTransform.Find("Close");
+        if (closeTranform == null)
+        {
+            Debug.LogError("ShopManager: Unable to find close transform");
+            return null;
+        }
+        return closeTranform.gameObject.GetComponent<Button>();
+    }
+
+    private void RemoveConfirmScreenButtonListeners(Button confirmButton, Button closeButton)
+    {
+        confirmButton.onClick.RemoveAllListeners();
+        closeButton.onClick.RemoveAllListeners();
+    }
+
+    private void PurchaseConfirmed()
+    {
+        TryPurchase();
     }
 }
