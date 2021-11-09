c2e5d282a42cd13931d744db740a11640e0b4e31
an978607 <anneb17@knights.ucf.edu>
2021-11-09 18:53:01 -0500

    Build Works!

---
+++ WaterKing/Assets/Scripts/ShopEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopEvent : MonoBehaviour
+{
+    PlayerDataManager player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDataManager>();
+    }
+
+    private void Promotion()
+    {
+        //always beneficial players gets 2.5% of their current cash (CAN ONLY BE BOUGHT ONCE)
+        //player.AddToCurrency(player.);
+    }
+   
+}
