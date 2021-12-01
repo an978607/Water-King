9293b2db46a40ba7da41c15d1a52b30ae538da6c
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-12-01 18:45:18 -0500

    Add get player information unit test

--- WaterKing/Assets/Tests/RemoteConnectionTests.cs
+++ WaterKing/Assets/Tests/RemoteConnectionTests.cs
@@ -12,11 +12,28 @@ namespace Tests
         [Test]
         public void Getting_Player_With_NonExisting_Email_Should_Receive_Player_ID_0()
         {
-            string strJSONInput =  "\"email\":\"notValidEmail@fake.com\"";
+            string strJSONInput =  "{\"email\":\"notValidEmail@fake.com\"}";
             string jsonResult = GetAPIDatabase.GetPlayers(strJSONInput);
             Player player = Deserialization.DeserializePlayer(jsonResult);
             Assert.IsNotNull(player);
             Assert.AreEqual(0, player.player_id);
+        }
+
+        [Test]
+        public void Should_Get_Player_Information()
+        {
+            string strJSONInput = "{\"email\":\"test1@example.com\"}";
+            string jsonResult = GetAPIDatabase.GetPlayers(strJSONInput);
+            Player player = Deserialization.DeserializePlayer(jsonResult);
+            Assert.IsNotNull(player);
+            Assert.AreEqual(1, player.player_id);
+            Assert.AreEqual(0, player.currency);
+            Assert.AreEqual(0, player.fuelAmount);
+            Assert.AreEqual("Bike", player.selectedVehicle);
+            Assert.AreEqual(0, player.upgrade);
+            Assert.AreEqual(0, player.protection);
+            Assert.AreEqual(0, player.time);
+            Assert.AreEqual(0, player.totalScore);
         }
     }
 }
